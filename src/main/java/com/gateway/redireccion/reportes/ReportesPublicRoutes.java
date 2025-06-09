71cc13bedeb06a662a94426fbceb98fa50591deb
Skibidi-Mate <mate.hernandezm@duocuc.cl>
2025-06-09 18:41:57 -0400

    se creo proxy y public para reportes

---
+++ src/main/java/com/gateway/redireccion/reportes/ReportesPublicRoutes.java
@@ -0,0 +1,10 @@
+package com.gateway.redireccion.reportes;
+
+
+public class ReportesPublicRoutes {
+    public static final String[] REPORTES_PUBLIC_GET = {
+        "/api/proxy/reportes",
+        "/api/proxy/reportes/"
+    };
+
+}
