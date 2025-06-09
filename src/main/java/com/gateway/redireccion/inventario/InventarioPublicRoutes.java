19dd28f1cb6d20acb3d947f78edddf732a3666a3
benjatussy <benja.contrerasg@duocuc.cl>
2025-06-09 18:26:08 -0400

    terminado la redireccion de inventario

--- src/main/java/com/gateway/redireccion/inventario/InventarioPublicRoutes.java
+++ src/main/java/com/gateway/redireccion/inventario/InventarioPublicRoutes.java
@@ -1,5 +1,9 @@
 package com.gateway.redireccion.inventario;
 
 public class InventarioPublicRoutes {
+        public static final String[] GESTION_PUBLIC_GET = {
+        "/api/proxy/inventario",
+        "/api/proxy/inventarios/"
+    };
 
 }
