7bd084fe681cd5e0983bd4c61d57e90ba83a8df3
benjatussy <benja.contrerasg@duocuc.cl>
2025-06-09 18:44:29 -0400

    arreglado las public routes

--- src/main/java/com/gateway/redireccion/envios/EnviosPublicRoutes.java
+++ src/main/java/com/gateway/redireccion/envios/EnviosPublicRoutes.java
@@ -2,7 +2,7 @@ package com.gateway.redireccion.envios;
 
 public class EnviosPublicRoutes {
     public static final String[] ENVIOS_PUBLIC_GET = {
-        "/api/proxy/envio",
+        "/api/proxy/envios",
         "/api/proxy/envios/"
     };
 
--- src/main/java/com/gateway/redireccion/inventario/InventarioPublicRoutes.java
+++ src/main/java/com/gateway/redireccion/inventario/InventarioPublicRoutes.java
@@ -2,7 +2,7 @@ package com.gateway.redireccion.inventario;
 
 public class InventarioPublicRoutes {
         public static final String[] INVENTARIO_PUBLIC_GET = {
-        "/api/proxy/inventario",
+        "/api/proxy/inventarios",
         "/api/proxy/inventarios/"
     };
 
